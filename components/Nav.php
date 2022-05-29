0bc3bb96951c8a6559f2be111289e394f17093e9
Senna Oudshoorn <94371224+soudshoorn@users.noreply.github.com>
2022-05-29 23:05:15 +0200

    added goals

--- components/Nav.php
+++ components/Nav.php
@@ -1,31 +1,37 @@
 <nav>
-    <div class="nav__wrapper--left">
-        <figure class="nav__img--wrapper">
-            <a href="/"></a><img src="./assets/img/healthone-logo.png" class="nav__img" alt=""></a>
-        </figure>
-        <ul class="nav__link--list">
-            <li class="nav__link">
-                <a href="#aboutme" class="
-                nav__link--anchor 
-                link__hover-effect 
-                ">Home</a>
-            </li>
-            <li class="nav__link">
-                <a href="#projects" class="
-                nav__link--anchor 
-                link__hover-effect 
-                ">Apparaten</a>
-            </li>
-            <li class="nav__link">
-                <a href="#contact" class="
-                nav__link--anchor 
-                link__hover-effect 
-                ">Contact</a>
-            </li>
-        </ul>
-    </div>
-    <div class="nav__wrapper--right">
-        <button class="nav__button"><i class="far fa-user-circle"></i>Inloggen</button>
-        <button class="btn">Registreren</button>
+    <div class="container">
+        <div class="row">
+            <div class="nav__wrapper">
+                <div class="nav__wrapper--left">
+                    <figure class="nav__img--wrapper">
+                        <a href="/"></a><img src="./assets/img/healthone-logo.png" class="nav__img" alt=""></a>
+                    </figure>
+                    <ul class="nav__link--list">
+                        <li class="nav__link">
+                            <a href="#aboutme" class="
+                            nav__link--anchor 
+                            link__hover-effect 
+                            ">Home</a>
+                        </li>
+                        <li class="nav__link">
+                            <a href="#projects" class="
+                            nav__link--anchor 
+                            link__hover-effect 
+                            ">Apparaten</a>
+                        </li>
+                        <li class="nav__link">
+                            <a href="#contact" class="
+                            nav__link--anchor 
+                            link__hover-effect 
+                            ">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="nav__wrapper--right">
+                    <button class="nav__button"><i class="far fa-user-circle"></i>Inloggen</button>
+                    <button class="btn">Registreren</button>
+                </div>
+            </div>
+        </div>
     </div>
 </nav>
--- index.php
+++ index.php
@@ -1,11 +1,7 @@
-<?php 
-
-?>
-
 <html>
     <head>
         <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="./assets/css/style.css?v=1">
     </head>
     <body>
         <section id="landing">
@@ -13,18 +9,55 @@
                 include_once('./components/Nav.php');
             ?>
             <div class="header">
-                <div class="header__description">
-                    <h1 class="header__description--title">De sportschool voor jouw gezondheid</h1>
-                    <p class="header__description--para">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-                        Earum minus repellendus laudantium voluptates, sapiente nemo facere placeat? 
-                        Officiis quod nisi velit? Natus voluptas non nisi! Nisi, alias! Voluptates, totam? Qui!
-                    </p>
-                    <button class="btn">Registreer je en laat je mening achter</button>
+                <div class="container">
+                    <div class="row">
+                        <div class="header__wrapper">
+                            <div class="header__description">
+                                <h1 class="header__description--title">De sportschool voor jouw gezondheid</h1>
+                                <p class="header__description--para">
+                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                                    Earum minus repellendus laudantium voluptates, sapiente nemo facere placeat? 
+                                    Officiis quod nisi velit? Natus voluptas non nisi! Nisi, alias! Voluptates, totam? Qui!
+                                </p>
+                                <div class="header__button">
+                                    <button class="btn">Registreer direct!</button>
+                                </div>
+                            </div>
+                            <figure class="header__banner">
+                                <img src="./assets/img/header-banner.jpg" class="header__banner--img" alt="">
+                            </figure>
+                        </div>
+                    </div>
                 </div>
-                <figure class="header__banner">
-                    <img src="./assets/img/header-banner.jpg" class="header__banner--img" alt="">
-                </figure>
+            </div>
+        </section>
+        <section class="goals">
+            <div class="container">
+                <div class="row">
+                    <div class="goals__wrapper">
+                        <div class="goals__goal">
+                            <i class="fas fa-dumbbell goals__icon"></i>
+                            <div class="goals__goal--description">
+                                <h1 class="goals__goal--title">Krachttraining</h1>
+                                <p class="goal__goal--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ut repellendus consectetur.</p>
+                            </div>
+                        </div>
+                        <div class="goals__goal">
+                            <i class="fas fa-bicycle goals__icon"></i>
+                            <div class="goals__goal--description">
+                                <h1 class="goals__goal--title">Cardio</h1>
+                                <p class="goal__goal--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ut repellendus consectetur.</p>
+                            </div>
+                        </div>
+                        <div class="goals__goal">
+                            <i class="fas fa-spa goals__icon"></i>
+                            <div class="goals__goal--description">
+                                <h1 class="goals__goal--title">Fysio</h1>
+                                <p class="goal__goal--para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ut repellendus consectetur.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </body>
