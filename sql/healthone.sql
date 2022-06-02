72fb1a54923f0cb9eb051bcb692a7652b1ec41c7
Senna Oudshoorn <302432559@student.rocmondriaan.nl>
2022-06-02 15:11:45 +0200

    added new sql file

--- sql/healthone.sql
+++ sql/healthone.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.3.0-dev+20220531.aadb8cc914
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 01 jun 2022 om 22:09
--- Serverversie: 10.4.24-MariaDB
--- PHP-versie: 8.1.5
+-- Gegenereerd op: 02 jun 2022 om 15:11
+-- Serverversie: 10.4.22-MariaDB
+-- PHP-versie: 7.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -60,7 +60,20 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `img`, `description`, `category_id`) VALUES
-(1, 'Loopband TX20', 'loopband-tx20.jpg', 'De cardiostrong TX20 is een compacte en betaalbare loopband op instapniveau om thuis wandel- en hardlooptrainingen te volgen. Omdat de loopband eenvoudig inklapbaar is en kleine afmetingen heeft kan hij zelfs in kleine fitnessruimtes en huiskamers gebruik', 2);
+(21, 'Loopband 56RX', '6298a92ceacc70.33156900.jpg', 'Met de 56RX Loopband heeft Bowflex een hoogwaardige loopband ontwikkeld die zorgt voor een natuurlijke hardloopervaring in je eigen huis. Zowel de brede loopmat (152x56cm) als de buitengewone helling, dragen bij aan een realistische loopervaring. De helli', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -79,6 +92,12 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT voor geëxporteerde tabellen
 --
 
@@ -92,12 +111,15 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT voor een tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-
-
