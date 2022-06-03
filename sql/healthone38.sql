1a0e1ae8a5f1584afaba680c832d4f13da064e29
Senna Oudshoorn <sennaoudshoorn@gmail.com>
2022-06-03 15:22:46 +0200

    added new sql file

--- sql/healthone38.sql
+++ sql/healthone38.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 03 jun 2022 om 15:22
+-- Gegenereerd op: 02 jun 2022 om 15:11
 -- Serverversie: 10.4.22-MariaDB
 -- PHP-versie: 7.4.27
 
@@ -51,7 +51,7 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `img` varchar(255) NOT NULL,
-  `description` text NOT NULL,
+  `description` varchar(255) NOT NULL,
   `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -60,29 +60,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `img`, `description`, `category_id`) VALUES
-(21, 'Loopband 56RX', '6298a92ceacc70.33156900.jpg', 'Met de 56RX Loopband heeft Bowflex een hoogwaardige loopband ontwikkeld die zorgt voor een natuurlijke hardloopervaring in je eigen huis. Zowel de brede loopmat (152x56cm) als de buitengewone helling, dragen bij aan een realistische loopervaring. De helli', 2),
-(27, 'Crosstrainer EX60', '629a0a45bddac4.79070594.jpg', 'De cardiostrong crosstrainer EX60 Touch staat voor stabiliteit, uitstekende bewegingskwaliteit, een stijlvol design en zeer comfortabel. Dankzij het compacte ontwerp is de EX60 erg ruimtebesparend en kan daarom vrijwel in elke woonkamer terecht. Met zijn twee vliegwielen voor en achter zet de crosstrainer nieuwe maatstaven in zijn klasse op het gebied van bewegingskwaliteit en biedt hij een optimale trainingservaring, zelfs in de kleinste ruimtes. Het elegante zwarte frame en de slijtvaste poedercoating geven de EX60 Touch een stijlvol uiterlijk.', 1);
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `reviews`
---
-
-CREATE TABLE `reviews` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `stars` int(11) NOT NULL,
-  `description` text NOT NULL,
-  `product_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `reviews`
---
-
-INSERT INTO `reviews` (`id`, `name`, `stars`, `description`, `product_id`) VALUES
-(5, 'Senna Guest', 0, 'Ja kut apparaat', 21);
+(21, 'Loopband 56RX', '6298a92ceacc70.33156900.jpg', 'Met de 56RX Loopband heeft Bowflex een hoogwaardige loopband ontwikkeld die zorgt voor een natuurlijke hardloopervaring in je eigen huis. Zowel de brede loopmat (152x56cm) als de buitengewone helling, dragen bij aan een realistische loopervaring. De helli', 2);
 
 -- --------------------------------------------------------
 
@@ -94,17 +72,8 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL DEFAULT 'guest'
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Gegevens worden geëxporteerd voor tabel `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
-(1, 'Senna Oudshoorn', 'sennaoudshoorn@gmail.com', 'qwerty', 'admin'),
-(2, 'Senna Guest', 'senna@guest.nl', 'qwerty', 'guest');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -120,12 +89,6 @@ ALTER TABLE `categories`
 -- Indexen voor tabel `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexen voor tabel `reviews`
---
-ALTER TABLE `reviews`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -148,19 +111,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT voor een tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-
---
--- AUTO_INCREMENT voor een tabel `reviews`
---
-ALTER TABLE `reviews`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
